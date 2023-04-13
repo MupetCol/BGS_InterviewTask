3734165f64c3d662224fc22b9cd02eb711893a1d
MupetCol <65467881+MupetCol@users.noreply.github.com>
2023-04-12 23:03:40 -0500

    Player Movement & Bounds
    - Added base player movement script (taken from another project)
    - Added camera bounds for cinemachine component
    - Added bounds for player collision with four invisible walls
    - Added some scripts of scriptable objects that might be used for a modular architecture later

---
+++ Assets/Scripts/BoolReference.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableReferences/BoolReference", fileName = "BoolVar")]
+public class BoolReference : ScriptableObject
+{
+	public bool toggle;
+	public bool reset;
+
+	[Header("0 = false, 1 = true")]
+	public int defValue = 0;
+
+	private void OnEnable()
+	{
+		hideFlags = HideFlags.DontUnloadUnusedAsset;
+		if (reset && defValue == 0)
+		{
+			toggle = false;
+		}
+		else if (reset && defValue == 1)
+		{
+			toggle = true;
+		}
+	}
+
+	public void Toggle()
+	{
+		toggle = !toggle;
+	}
+}
